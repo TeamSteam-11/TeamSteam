081f70ce387ef71c9b1770702352c06119d29514
bw1611 <bw1611@naver.com>
2023-06-21 13:36:48 +0900

    init commit

---
+++ src/main/java/com/ll/TeamSteam/TeamSteamApplication.java
@@ -0,0 +1,13 @@
+package com.ll.TeamSteam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamSteamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamSteamApplication.class, args);
+	}
+
+}
