a93e829ccefaeba5ba730b5acb5e74858ca750a0
깃wooqqq <sge0225@naver.com>
2023-06-26 16:01:25 +0900

    feat : 매칭 서비스에 매칭 등록 메서드 추가

--- src/main/java/com/ll/TeamSteam/domain/matching/service/MatchingService.java
+++ src/main/java/com/ll/TeamSteam/domain/matching/service/MatchingService.java
@@ -1,10 +1,28 @@
 package com.ll.TeamSteam.domain.matching.service;
 
+import com.ll.TeamSteam.domain.matching.entity.Matching;
+import com.ll.TeamSteam.domain.matching.repository.MatchingRepository;
+import com.ll.TeamSteam.global.rsData.RsData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class MatchingService {
+    public final MatchingRepository matchingRepository;
+
+    // 매칭 등록 기능
+    public RsData<Matching> create(String title, String content, Long capacity) {
+        Matching matching = Matching
+                .builder()
+                .title(title)
+                .content(content)
+                .capacity(capacity)
+                .build();
+
+        matchingRepository.save(matching);
+
+        return RsData.of("S-1", "입력하신 매칭이 등록되었습니다.");
+    }
 
 }
