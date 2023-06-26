842a8f6919e2d20f770508bbad7fab2c4cd71cc0
wooqqq <118504257+wooqqq@users.noreply.github.com>
2023-06-26 14:32:14 +0900

    Feature/5 Navbar 임시 적용 (#8)
    * feat : 공통 JS, CSS 추가 및 공통 레이아웃에 navbar 추가
    
    * feat : navbar 디자인 설정
    
    * feat : 매칭 목록 페이지 생성
    
    * design : 레이아웃 및 매칭 목록 수정
    
    * remove : application-secret.yml 삭제

--- src/main/java/com/ll/TeamSteam/TeamSteamApplication.java
+++ src/main/java/com/ll/TeamSteam/TeamSteamApplication.java
@@ -2,8 +2,9 @@ package com.ll.TeamSteam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class TeamSteamApplication {
 
 	public static void main(String[] args) {
