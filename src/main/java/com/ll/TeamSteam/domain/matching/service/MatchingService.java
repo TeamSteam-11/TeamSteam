9ae73305b3cef6c7c5a02f6a16cb9b154356f45e
깃wooqqq <sge0225@naver.com>
2023-06-26 15:39:54 +0900

    feat : 매칭 서비스 및 리포지토리 생성

---
+++ src/main/java/com/ll/TeamSteam/domain/matching/service/MatchingService.java
@@ -0,0 +1,10 @@
+package com.ll.TeamSteam.domain.matching.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MatchingService {
+
+}
