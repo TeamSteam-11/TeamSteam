43b420a0ff5be0a0fb70a033eef0737e997f833b
bw1611 <97327583+leebuwon@users.noreply.github.com>
2023-06-28 13:17:23 +0900

    chore : 쓸모없는 import문 제거 (#18)

--- src/main/java/com/ll/TeamSteam/domain/home/HomeController.java
+++ src/main/java/com/ll/TeamSteam/domain/home/HomeController.java
@@ -1,12 +1,9 @@
 package com.ll.TeamSteam.domain.home;
 
 import com.ll.TeamSteam.global.rq.Rq;
-import com.ll.TeamSteam.global.security.SecurityUser;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
