01e6a562fa19d9066c142814c752b80ade946595
Beconfident01 <hom0726@naver.com>
2023-06-28 15:01:19 +0900

    refactor : UserController 리팩토링

--- src/main/java/com/ll/TeamSteam/domain/user/controller/UserController.java
+++ src/main/java/com/ll/TeamSteam/domain/user/controller/UserController.java
@@ -15,14 +15,10 @@ import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.util.UriComponentsBuilder;
 
-import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -45,63 +41,7 @@ public class UserController {
     }
 
     @GetMapping("/login/check")
-    public String check(
-            @RequestParam(value = "openid.ns") String openidNs,
-            @RequestParam(value = "openid.mode") String openidMode,
-            @RequestParam(value = "openid.op_endpoint") String openidOpEndpoint,
-            @RequestParam(value = "openid.claimed_id") String openidClaimedId,
-            @RequestParam(value = "openid.identity") String openidIdentity,
-            @RequestParam(value = "openid.return_to") String openidReturnTo,
-            @RequestParam(value = "openid.response_nonce") String openidResponseNonce,
-            @RequestParam(value = "openid.assoc_handle") String openidAssocHandle,
-            @RequestParam(value = "openid.signed") String openidSigned,
-            @RequestParam(value = "openid.sig") String openidSig,
-            HttpSession session
-    ) {
-
-        UriComponentsBuilder builder = UriComponentsBuilder.fromUriString("https://steamcommunity.com/openid/login")
-                .queryParam("openid.ns", openidNs)
-                .queryParam("openid.mode", "check_authentication")
-                .queryParam("openid.op_endpoint", openidOpEndpoint)
-                .queryParam("openid.claimed_id", openidClaimedId)
-                .queryParam("openid.identity", openidIdentity)
-                .queryParam("openid.return_to", openidReturnTo)
-                .queryParam("openid.response_nonce", openidResponseNonce)
-                .queryParam("openid.assoc_handle", openidAssocHandle)
-                .queryParam("openid.signed", openidSigned)
-                .queryParam("openid.sig", openidSig);
-
-
-
-        String block = WebClient.create("https://steamcommunity.com")
-                .get()
-                .uri(uriBuilder -> uriBuilder
-                        .path("/openid/login")
-                        .queryParam("openid.ns", openidNs)
-                        .queryParam("openid.mode", "check_authentication")
-                        .queryParam("openid.op_endpoint", openidOpEndpoint)
-                        .queryParam("openid.claimed_id", openidClaimedId)
-                        .queryParam("openid.identity", openidIdentity)
-                        .queryParam("openid.return_to", openidReturnTo)
-                        .queryParam("openid.response_nonce", openidResponseNonce)
-                        .queryParam("openid.assoc_handle", openidAssocHandle)
-                        .queryParam("openid.signed", openidSigned)
-                        .queryParam("openid.sig", openidSig)
-                        .build()
-                )
-                .retrieve()
-                .bodyToMono(String.class)
-                .block();
-        System.out.println(block);
-
-        boolean isTrue = Objects.requireNonNull(block).contains("true");
-
-        // 회원가입
-            // 1. findBySteamId(steamId)
-            // 2. 없으면 회원가입 Member or 로그인
-        // 회원가입 안 할 때
-            // security 객체를 만들고 session에 저장
-
+    public String startSession( @RequestParam(value = "openid.identity") String openidIdentity, HttpSession session ) {
 
         Pattern pattern = Pattern.compile("\\d+");
         Matcher matcher = pattern.matcher(openidIdentity);
--- src/main/java/com/ll/TeamSteam/domain/user/service/UserService.java
+++ src/main/java/com/ll/TeamSteam/domain/user/service/UserService.java
@@ -1,15 +1,24 @@
 package com.ll.TeamSteam.domain.user.service;
 
+import static org.springframework.security.web.context.HttpSessionSecurityContextRepository.*;
+
 import com.ll.TeamSteam.domain.user.entity.Gender;
 import com.ll.TeamSteam.domain.user.entity.User;
 import com.ll.TeamSteam.domain.user.repository.UserRepository;
+import com.ll.TeamSteam.global.security.SecurityUser;
 import com.ll.TeamSteam.global.security.UserInfoResponse;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Optional;
-
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 @RequiredArgsConstructor
@@ -44,7 +53,4 @@ public class UserService {
 
         return user;
     }
-
-
-
 }
