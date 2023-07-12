d0d7201ec1ad3e27cf710ae6f4d382759164ee30
Beconfident01 <hom0726@naver.com>
2023-07-12 20:07:34 +0900

    design : 게임태그저장, 게임라이브러리 수정

--- src/main/java/com/ll/TeamSteam/domain/user/controller/UserController.java
+++ src/main/java/com/ll/TeamSteam/domain/user/controller/UserController.java
@@ -175,7 +175,7 @@ public class UserController {
     @GetMapping(value = "/user/createGameTag", produces = MediaType.TEXT_HTML_VALUE)
     public String userGameListSave(
         @RequestParam(defaultValue = "0") int page,
-        @RequestParam(defaultValue = "9") int size,
+        @RequestParam(defaultValue = "15") int size,
         @AuthenticationPrincipal SecurityUser user, Model model
         ) throws ParseException {
 
@@ -274,7 +274,7 @@ public class UserController {
     @GetMapping(value = "/user/profile/{userId}", produces = MediaType.TEXT_HTML_VALUE)
     public String userProfile(@PathVariable long userId, @AuthenticationPrincipal SecurityUser user, Model model,
         @RequestParam(defaultValue = "0") int page,
-        @RequestParam(defaultValue = "9") int size) throws ParseException {
+        @RequestParam(defaultValue = "15") int size) throws ParseException {
 
 
         User targetUser = userService.findById(userId).orElseThrow();
