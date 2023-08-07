e05d9ef4bc15be4b8761738cd722ea4e469f0a8d
깃wooqqq <sge0225@naver.com>
2023-08-08 03:52:38 +0900

    test : modify 테스트코드 추가

--- src/test/java/com/ll/TeamSteam/domain/matching/service/MatchingServiceTest.java
+++ src/test/java/com/ll/TeamSteam/domain/matching/service/MatchingServiceTest.java
@@ -5,13 +5,18 @@ import com.ll.TeamSteam.domain.matching.repository.MatchingRepository;
 import com.ll.TeamSteam.domain.matchingTag.entity.GenreTagType;
 import com.ll.TeamSteam.domain.user.entity.User;
 import com.ll.TeamSteam.domain.user.service.UserService;
+import com.ll.TeamSteam.global.rsData.RsData;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.as;
+import static org.mockito.Mockito.*;
 
 import java.time.LocalDateTime;
 
@@ -26,6 +31,7 @@ public class MatchingServiceTest {
     @Autowired
     private MatchingService matchingService;
     @Autowired
+    @MockBean
     private MatchingRepository matchingRepository;
 
     @Test
@@ -62,7 +68,7 @@ public class MatchingServiceTest {
     }
 
     @Test
-    @DisplayName("calculateDeadline - selectedHours가 0인 경우")
+    @DisplayName("calculateDeadline - selectedHours가 0 이하인 경우")
     void t004() {
         // selectedHours가 0 이하인 경우 testTime(실제로는 modifyDate)에 Days 단위로 30일이 더해져야 함(default)
         LocalDateTime testTime = LocalDateTime.now();
@@ -71,6 +77,82 @@ public class MatchingServiceTest {
         LocalDateTime deadlineTest = matchingService.calculateDeadline(testTime, selectedHours);
 
         assertThat(deadlineTest).isEqualTo(testTime.plusDays(30));
+    }
+
+    @Test
+    @DisplayName("modify - selectedHours가 1 이상인 경우")
+    void t005() {
+        User user1 = userService.findById(1L).get();
+        LocalDateTime testTime = LocalDateTime.now();
+
+        Matching matching1 = matchingService.create(user1, "matchingTest1", "content1", GenreTagType.격투, 41000, "남성", 3L, 5, 8, testTime);
+
+        int selectedHours = 5;
+
+        when(matchingRepository.save(any(Matching.class))).thenReturn(matching1);
+
+        // WHEN
+        RsData<Matching> result = matchingService.modify(
+                matching1,
+                "newTitle",
+                "newContent",
+                GenreTagType.군사,
+                "여성",
+                4L,
+                10,
+                15,
+                selectedHours
+        );
+
+        // THEN
+        assertThat(result.getData()).isEqualTo(matching1);
+        assertThat(matching1.getTitle()).isEqualTo("newTitle");
+        assertThat(matching1.getContent()).isEqualTo("newContent");
+        assertThat(matching1.getGenre()).isEqualTo(GenreTagType.군사);
+        assertThat(matching1.getGender()).isEqualTo("여성");
+        assertThat(matching1.getCapacity()).isEqualTo(4L);
+        assertThat(matching1.getStartTime()).isEqualTo(10);
+        assertThat(matching1.getEndTime()).isEqualTo(15);
+        // modify 메서드에서는 modifyDate가 실행 시점으로 저장이 되기에 테스트코드 실행 시 초 단위가 맞지 않아 실패
+        // assertThat(matching1.getDeadlineDate()).isEqualTo(testTime.plusHours(5));
+    }
+
+    @Test
+    @DisplayName("modify - selectedHours가 0 이하인 경우")
+    void t006() {
+        User user1 = userService.findById(1L).get();
+        LocalDateTime testTime = LocalDateTime.now();
+
+        Matching matching1 = matchingService.create(user1, "matchingTest1", "content1", GenreTagType.격투, 41000, "남성", 3L, 5, 8, testTime);
+
+        int selectedHours = 0;
+
+        when(matchingRepository.save(any(Matching.class))).thenReturn(matching1);
+
+        // WHEN
+        RsData<Matching> result = matchingService.modify(
+                matching1,
+                "newTitle",
+                "newContent",
+                GenreTagType.군사,
+                "여성",
+                4L,
+                10,
+                15,
+                selectedHours
+        );
+
+        // THEN
+        assertThat(result.getData()).isEqualTo(matching1);
+        assertThat(matching1.getTitle()).isEqualTo("newTitle");
+        assertThat(matching1.getContent()).isEqualTo("newContent");
+        assertThat(matching1.getGenre()).isEqualTo(GenreTagType.군사);
+        assertThat(matching1.getGender()).isEqualTo("여성");
+        assertThat(matching1.getCapacity()).isEqualTo(4L);
+        assertThat(matching1.getStartTime()).isEqualTo(10);
+        assertThat(matching1.getEndTime()).isEqualTo(15);
+        // modify 메서드에서는 modifyDate가 실행 시점으로 저장이 되기에 테스트코드 실행 시 초 단위가 맞지 않아 실패
+        // assertThat(matching1.getDeadlineDate()).isEqualTo(testTime.plusDays(30));
     }
 
 }
