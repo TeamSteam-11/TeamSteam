5050c9e6df0fc0927f97b25af92a034d59265f0e
tkdtn4657 <tkdtn101226@gmail.com>
2023-09-12 16:46:33 +0900

    feat : 나의 랭크가 7위 밖일 때 리더보드에 나의 순위 표시하는 백로직 구현

--- src/main/java/com/ll/TeamSteam/domain/rank/controller/RankController.java
+++ src/main/java/com/ll/TeamSteam/domain/rank/controller/RankController.java
@@ -3,14 +3,19 @@ package com.ll.TeamSteam.domain.rank.controller;
 import com.ll.TeamSteam.domain.rank.service.RankService;
 import com.ll.TeamSteam.domain.user.controller.UserController;
 import com.ll.TeamSteam.domain.user.entity.User;
+import com.ll.TeamSteam.global.rq.Rq;
+import com.ll.TeamSteam.global.security.SecurityUser;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
@@ -22,18 +27,35 @@ public class RankController {
     private final RankService rankService;
 
     @GetMapping("/rank")
-    public String showRank(Model model){
+    public String showRank(Model model, @AuthenticationPrincipal SecurityUser user) {
         List<User> topSevenUserList = getTopSevenUsersWithHighTemperature();
-//        List<User> oneToThreeUserList = new ArrayList<>(topSevenUserList.subList(0, 3));
-//        List<User> fourToSevenUserList = new ArrayList<>(topSevenUserList.subList(3, topSevenUserList.size()));
-
-//        model.addAttribute("fourToSevenUserList", fourToSevenUserList);
-//        model.addAttribute("oneToThreeUserList", oneToThreeUserList);
         model.addAttribute("topSevenUserList", topSevenUserList);
+
+        if(user == null){ // 미로그인 상태
+            return "rank/rank";
+        }
+
+        //리더보드 최상단에 포함되는지 확인 추후 스트림 사용가능할 때 리팩토링
+        boolean listContainUser = false;
+        for (User a : topSevenUserList) {
+            if (a.getId() == user.getId()) {
+                listContainUser = true;
+            }
+        }
+
+        /**
+         * 나의 랭크 인덱스 표시하기 구현중
+         */
+        long userRank = rankService.getMyRank(user.getId());
+        if (!listContainUser) {
+            model.addAttribute("myRank", userRank);
+            model.addAttribute("myRankProfile", user);
+        }
+
         return "rank/rank";
     }
 
-    public List<User> getTopSevenUsersWithHighTemperature (){
+    public List<User> getTopSevenUsersWithHighTemperature() {
         return rankService.getTopSeven();
     }
 
--- src/main/java/com/ll/TeamSteam/domain/rank/service/RankService.java
+++ src/main/java/com/ll/TeamSteam/domain/rank/service/RankService.java
@@ -7,7 +7,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -20,4 +22,20 @@ public class RankService {
     public List<User> getTopSeven() {
         return userRepository.findTop7ByOrderByTemperatureDesc();
     }
+
+    /**
+     * @param id
+     * @return <UserRank, User>
+     */
+    public Long getMyRank(Long id){
+        List<User> userList = userRepository.findAllByOrderByTemperatureDesc();
+
+        User user = userRepository.findById(id).orElseThrow();
+        long myRankNum = userList.indexOf(user);
+
+//        Map<Long, User> myRank = new HashMap<>();
+//        myRank.put(myRankNum, user);
+        return myRankNum;
+    }
+
 }
