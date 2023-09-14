2ffc27fabe137c24cbf9d8c527148b787a764ba6
tkdtn4657 <75903442+tkdtn4657@users.noreply.github.com>
2023-09-14 13:47:18 +0900

    Ts-38 명예의전당 페이지 내순위 출력, 디자인 변경 (#236)
    * design : 명예의전당 페이지 1차 수정 - 1,2,3등의 미니프로필 제거, 피그마와 색상 통일
    
    * design : 명예의전당 비율수정 및 온도 텍스트 추가
    
    * feat : 명예의전당 테스트 더미데이터 생성버튼 추가 및 백엔드 코드 추가
    
    * feat : 나의 랭크가 7위 밖일 때 리더보드에 나의 순위 표시하는 백로직 구현
    
    * feat : 7명 출력 -> 10명 출력 + 본인이 10위 바깥이라면 본인도 출력되도록 구현, 전반적인 디자인 수정
    
    * design : 10명제한출력 및 10위 안에 로그인한 본인이 없을 때 내순위 하단에 출력
    
    * comment : 메서드명 명확하게 변경, 메서드 주석 수정

--- src/main/java/com/ll/TeamSteam/domain/rank/controller/RankController.java
+++ src/main/java/com/ll/TeamSteam/domain/rank/controller/RankController.java
@@ -3,13 +3,15 @@ package com.ll.TeamSteam.domain.rank.controller;
 import com.ll.TeamSteam.domain.rank.service.RankService;
 import com.ll.TeamSteam.domain.user.controller.UserController;
 import com.ll.TeamSteam.domain.user.entity.User;
+import com.ll.TeamSteam.domain.user.service.UserService;
+import com.ll.TeamSteam.global.security.SecurityUser;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -18,22 +20,38 @@ import java.util.List;
 public class RankController {
 
     private final UserController userController;
+    private final UserService userService;
 
     private final RankService rankService;
 
     @GetMapping("/rank")
-    public String showRank(Model model){
-        List<User> topSevenUserList = getTopSevenUsersWithHighTemperature();
-        List<User> oneToThreeUserList = new ArrayList<>(topSevenUserList.subList(0, 3));
-        List<User> fourToSevenUserList = new ArrayList<>(topSevenUserList.subList(3, topSevenUserList.size()));
+    public String showRank(Model model, @AuthenticationPrincipal SecurityUser user) {
+        List<User> topUserList = getTopUsersWithHighTemperature();
+        model.addAttribute("topUserList", topUserList);
 
-        model.addAttribute("fourToSevenUserList", fourToSevenUserList);
-        model.addAttribute("oneToThreeUserList", oneToThreeUserList);
+        if (user == null) { // 미로그인 상태
+            return "rank/rank";
+        }
+
+        //리더보드 최상단에 포함되는지 확인 추후 스트림 사용가능할 때 리팩토링
+        boolean listContainUser = false;
+        for (User a : topUserList) {
+            if (a.getId() == user.getId()) {
+                listContainUser = true;
+            }
+        }
+
+        long userRank = rankService.getUserOfTemperatureRank(user.getId());
+        if (!listContainUser) {
+            model.addAttribute("myRank", userRank);
+            model.addAttribute("myRankProfile", userService.findById(user.getId()).orElseThrow());
+        }
+
         return "rank/rank";
     }
 
-    public List<User> getTopSevenUsersWithHighTemperature (){
-        return rankService.getTopSeven();
+    public List<User> getTopUsersWithHighTemperature() {
+        return rankService.getTopUserList();
     }
 
 }
