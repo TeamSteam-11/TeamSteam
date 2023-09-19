004ea324372ebfb0cb79d81a61098e2762006b4d
bw1611 <bw1611@naver.com>
2023-09-19 16:20:00 +0900

    design : 생성, 수정 default 값 넣기

--- src/main/java/com/ll/TeamSteam/domain/matching/controller/MatchingController.java
+++ src/main/java/com/ll/TeamSteam/domain/matching/controller/MatchingController.java
@@ -114,7 +114,7 @@ public class MatchingController {
                 createForm.getStartTime(),
                 createForm.getEndTime(),
                 deadlineDate,
-                createForm.isMic()
+                createForm.getIsMic()
         );
 
         // 매칭 등록 실패 시
@@ -197,7 +197,7 @@ public class MatchingController {
         }
 
         RsData<Matching> modifyRsData = matchingService.modify(matching, createForm.getTitle(), createForm.getContent(),
-                createForm.getGenre(), createForm.getGender(), createForm.getCapacity(), createForm.getStartTime(), createForm.getEndTime(), createForm.getSelectedHours(), createForm.isMic());
+                createForm.getGenre(), createForm.getGender(), createForm.getCapacity(), createForm.getStartTime(), createForm.getEndTime(), createForm.getIsMic());
 
         chatRoomService.updateChatRoomName(matching.getChatRoom(), matching.getTitle());
 
