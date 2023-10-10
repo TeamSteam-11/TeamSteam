8fbd69ed3a16743eb57b5f63354ab317f59f1154
bw1611 <bw1611@naver.com>
2023-10-10 15:41:48 +0900

    bug : Xss scrpit 방어

--- src/main/java/com/ll/TeamSteam/domain/dmMessage/service/DmMessageService.java
+++ src/main/java/com/ll/TeamSteam/domain/dmMessage/service/DmMessageService.java
@@ -8,6 +8,7 @@ import com.ll.TeamSteam.domain.dmMessage.repository.DmMessageRepository;
 import com.ll.TeamSteam.domain.dmUser.entity.DmUser;
 import com.ll.TeamSteam.domain.dmUser.service.DmUserService;
 import com.ll.TeamSteam.domain.user.entity.User;
+import com.ll.TeamSteam.global.filter.CleanXss;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.bson.types.ObjectId;
@@ -33,13 +34,15 @@ public class DmMessageService {
 
         Dm dm = dmService.findByDmId(dmId);
 
+        String cleanContent = CleanXss.replaceXssAnsSqlInjection(content);
+
         if (sender == null){
             throw new IllegalArgumentException("너 로그인 안했어~!");
         }
 
         dmUserService.findDmUserByUserId(dm, sender.getId());
 
-        DmMessage dmMessage = DmMessage.create(content, sender.getId(), sender.getUsername(), sender.getAvatar(), dm.getId());
+        DmMessage dmMessage = DmMessage.create(cleanContent, sender.getId(), sender.getUsername(), sender.getAvatar(), dm.getId());
 
         dmMessage.validateLength(dmMessage.getContent());
 
